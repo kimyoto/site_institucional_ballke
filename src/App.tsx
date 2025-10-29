d9c449e5eac8979218692e5ea89e6268f994b891
Iruan <iruan.menegat@aluno.unc.br>
2025-10-28 22:36:57 -0300

    Conclusão do Footer e da primeira pagina

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
 
       <main>
         
-        {/* Seção 1 (background1) */}
+     
         <section className="hero-section">
           
-          {/* COLOQUE O TEXTO AQUI DENTRO */}
+          
           <h2 className="text-bg2">
             Confira nossas marcas e descubra qual é a ideal para você.
           </h2>
@@ -33,8 +33,7 @@ function App() {
             <img 
             src={require('./assets/nestsafe_banco.png')} 
             alt="Logo Nestsafe" 
-            className="hero-image-logo logo-1" 
- />
+            className="hero-image-logo logo-1" />
 
           {}
           <img 
@@ -44,23 +43,71 @@ function App() {
           />
         </section>
 
-        {/* Seção 2 (background2) */}
+       
         <section className="section-bg-two">
-          {/* A outra frase (que você ia adicionar) virá aqui depois */}
         </section>
         <section className="section-bg-three">
-           {/* Conteúdo da Seção 3 virá aqui */}
            <h2 className="bg-three-text">
             Nossa trajetória até aqui
-          </h2>
+            </h2>
 
-          <a href="#link-conheca" className="conheca-button button-3">
+            <a href="#link-conheca" className="conheca-button button-3">
              Conheça
-          </a>
-
-
+            </a>
+        
             </section>
+
+            <section className="section-bg-four">
+              <h2 className="bg-four-text">
+              Nossas áreas de atuação
+              </h2>
+              <a href="#saiba-mais" className="saiba-mais-button button-4">
+              Saiba Mais
+              </a>
+            </section>
+
+        <section className="section-bg-five">
+          <h2 className="bg-five-text">
+            Trabalhe Conosco
+          </h2>
+          <a href="#vagas" className="vagas-button button-5">
+            Vagas Disponíveis
+          </a>
+        </section>
+
       </main>
+
+        <footer className="footer-container">
+        
+<div className="footer-bar-gray">
+            <div className="footer-social-group">
+              <a href="https://www.facebook.com/grupoballke/" target="_blank" rel="noopener noreferrer">
+                <img src={require('./assets/lg_facebook.png')} alt="Facebook" className="footer-social-icon" />
+              </a>
+              <a href="https://www.instagram.com/grupoballke/" target="_blank" rel="noopener noreferrer">
+                <img src={require('./assets/lg_instagram.png')} alt="Instagram" className="footer-social-icon" />
+              </a>
+              <a href="https://www.linkedin.com/company/grupo-ballke/posts/?feedView=all" target="_blank" rel="noopener noreferrer">
+                <img src={require('./assets/lg_linkedin.png')} alt="LinkedIn" className="footer-social-icon" />
+              </a>
+            </div>
+              <a href="#contato" className="footer-link link-contato">Contato</a>
+  
+              <a href="/servicos" className="footer-link link-servicos">Serviços</a>
+  
+              <a href="/carreiras" className="footer-link link-carreira">Carreira</a>
+
+            </div>
+
+          <div className="footer-bar-white">
+            <img 
+              src={require('./assets/logo_ballke_escuro.png')} 
+              alt="Logo Grupo Ballke"
+              className="footer-logo"
+            />
+          </div>
+
+        </footer>
     </div>
   );
 }
