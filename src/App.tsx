9d8dc4dd179a19aeba27c28d4f2d9022b7867cb8
Iruan <iruan.menegat@aluno.unc.br>
2025-11-20 15:51:18 -0300

    mover footer pro seu proprio componete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ConhecaPage from "./pages/ConhecaPage";
@@ -23,65 +23,6 @@ function App() {
         <Route path="/carreiras" element={<CarreirasPage />} />
         <Route path="/contato" element={<ContatoPage />} />
       </Routes>
-
-      <footer className="footer-container">
-        <div className="footer-bar-gray">
-          <div className="footer-social-group">
-            <a
-              href="https://www.facebook.com/grupoballke/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={require("./assets/lg_facebook.png")}
-                alt="Facebook"
-                className="footer-social-icon"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/grupoballke/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={require("./assets/lg_instagram.png")}
-                alt="Instagram"
-                className="footer-social-icon"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/grupo-ballke/posts/?feedView=all"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={require("./assets/lg_linkedin.png")}
-                alt="LinkedIn"
-                className="footer-social-icon"
-              />
-            </a>
-          </div>
-          <a href="/contato" className="footer-link link-contato">
-            Contato
-          </a>
-
-          <a href="/marcas" className="footer-link link-servicos">
-            Marcas
-          </a>
-
-          <a href="/carreiras" className="footer-link link-carreira">
-            Carreira
-          </a>
-        </div>
-
-        <div className="footer-bar-white">
-          <img
-            src={require("./assets/logo_ballke_escuro.png")}
-            alt="Logo Grupo Ballke"
-            className="footer-logo"
-          />
-        </div>
-      </footer>
     </div>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  <footer className="footer-container">
+    <div className="footer-bar-gray">
+      <div className="footer-social-group">
+        <a
+          href="https://www.facebook.com/grupoballke/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={require("./assets/lg_facebook.png")}
+            alt="Facebook"
+            className="footer-social-icon"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/grupoballke/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={require("./assets/lg_instagram.png")}
+            alt="Instagram"
+            className="footer-social-icon"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/grupo-ballke/posts/?feedView=all"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={require("./assets/lg_linkedin.png")}
+            alt="LinkedIn"
+            className="footer-social-icon"
+          />
+        </a>
+      </div>
+      <a href="/contato" className="footer-link link-contato">
+        Contato
+      </a>
+
+      <a href="/marcas" className="footer-link link-servicos">
+        Marcas
+      </a>
+
+      <a href="/carreiras" className="footer-link link-carreira">
+        Carreira
+      </a>
+    </div>
+
+    <div className="footer-bar-white">
+      <img
+        src={require("./assets/logo_ballke_escuro.png")}
+        alt="Logo Grupo Ballke"
+        className="footer-logo"
+      />
+    </div>
+  </footer>;
+}
