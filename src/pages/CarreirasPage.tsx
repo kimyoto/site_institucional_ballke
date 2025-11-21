44b546855b2d2766622e73ef8b198ae4df043413
Iruan <iruan.menegat@aluno.unc.br>
2025-11-21 01:16:59 -0300

    carreias responsiva

--- src/pages/CarreirasPage.tsx
+++ src/pages/CarreirasPage.tsx
@@ -4,11 +4,11 @@ import "./CarreirasPage.css";
 function CarreirasPage() {
   return (
     <>
-      <section className="carreiras-hero-background">
+      <section className="section-container carreiras-hero-background">
         <h2 className="carreiras-hero-title">Carreiras</h2>
       </section>
 
-      <section className="carreiras-text">
+      <section className="section-container carreiras-text">
         <h2 className="carreiras-text1">
           Faça parte de uma equipe que transforma desafios em inovação e promove
           soluções sustentáveis para um mundo melhor. No Grupo Ballke,
@@ -21,11 +21,11 @@ function CarreirasPage() {
         </h2>
       </section>
 
-      <section className="vagas-hero">
+      <section className="section-container vagas-hero">
         <h2 className="vagas">Vagas</h2>
       </section>
 
-      <section className="vagas-lista-container">
+      <section className="section-container vagas-lista-container">
         <div className="vaga-item">
           <span className="vaga-titulo">Estoquista</span>
           <span className="vaga-local">Concórdia - SC</span>
