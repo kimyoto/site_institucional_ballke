29496a7b47f37265764427fc7887e45381820b4c
Iruan <iruan.menegat@aluno.unc.br>
2025-11-21 01:03:29 -0300

    quem somos responsivo

--- src/pages/QuemSomosPage.tsx
+++ src/pages/QuemSomosPage.tsx
@@ -5,24 +5,24 @@ import { Link } from "react-router-dom";
 function QuemSomosPage() {
   return (
     <>
-      <section className="quemsomos-hero-background">
+      <section className="section-container quemsomos-hero-background">
         <h2 className="text-topo-quemsomos">Quem Somos</h2>
       </section>
 
-      <section className="text-images-quemsomos">
+      <section className="section-container text-images-quemsomos">
         <h2 className="quemsomos-grupoballke">Grupo Ballke</h2>
-        <h2 className="quemsomos-text-1">
+        <h2 className="quemsomos-text">
           Nosso propósito é impulsionar a melhoria contínua dos serviços de
           saúde e estimular a inovação no mercado. Atuamos fornecendo produtos e
           soluções que apoiam profissionais e instituições na conquista de
           resultados cada vez mais eficientes e relevantes no cenário nacional.
         </h2>
-        <h2 className="quemsomos-text-2">
+        <h2 className="quemsomos-text">
           Além disso, trabalhamos em conformidade com as regulamentações
           nacionais e internacionais, garantindo que todos os nossos produtos
           estejam em total conformidade com os mais altos padrões de qualidade.
         </h2>
-        <h2 className="quemsomos-text-3">
+        <h2 className="quemsomos-text">
           No Grupo Ballke, cada produto reflete nosso compromisso com a
           inovações, a precisão e o cuidado com a saúde. Convidamos você a
           explorar nossas soluções e ver como podemos agregar valor aos seus
@@ -31,52 +31,67 @@ function QuemSomosPage() {
         </h2>
       </section>
 
-      <section className="quemsomos-images"></section>
-      <div className="gray-bar-atuacao">
-        <h2 className="text-images-1">NOSSAS ÁREAS DE ATUAÇÃO</h2>
-      </div>
-
-      <section className="quemsomos-two-column-section">
-        <div className="column-left-block">
-          <img src={require("../assets/quemsomos2.png")} alt="Nossa Unidade" />
-          <div className="image-gray-bar">
-            <h3>NOSSA UNIDADE</h3>
+      <section className="section-container quemsomos-images">
+        <div>
+          <img
+            src={require("../assets/quemsomos1.png")}
+            alt="Nossas áreas de atuação"
+            className="quemsomos-section-imgs"
+          ></img>
+          <div className="gray-bar-atuacao">
+            <h2 className="text-images-1">NOSSAS ÁREAS DE ATUAÇÃO</h2>
           </div>
         </div>
 
-        <Link to="/nossa-cultura" className="link-block-wrapper">
-          <div className="column-right-block">
+        <div className="quemsomos-duas-colunas">
+          <div className="coluna">
             <img
-              src={require("../assets/quemsomos3.png")}
-              alt="Nossa Cultura"
+              src={require("../assets/quemsomos2.png")}
+              alt="Nossa Unidade"
+              className="quemsomos-section-imgs"
             />
             <div className="image-gray-bar">
-              <h3>Nossa cultura</h3>
+              <h2 className="text-images-1">NOSSA UNIDADE</h2>
             </div>
           </div>
-        </Link>
+          
+          <div className="coluna">
+            <Link to="/nossa-cultura" className="link-block-wrapper">
+              <img
+                src={require("../assets/quemsomos3.png")}
+                alt="Nossa Cultura"
+                className="quemsomos-section-imgs"
+              />
+              <div className="image-gray-bar">
+                <h2 className="text-images-1">NOSSA CULTURA</h2>
+              </div>
+            </Link>
+          </div>
+        </div>
       </section>
 
-      <section className="parceiros-section">
+      <section className="section-container parceiros-section">
         <h2 className="parceiros-title">Parceiros</h2>
 
-        <img
-          src={require("../assets/meta.png")}
-          alt="Logo Meta"
-          className="parceiros-logo logo-meta"
-        />
+        <div className="logos-container">
+          <img
+            src={require("../assets/meta.png")}
+            alt="Logo Meta"
+            className="parceiros-logo logo-meta"
+          />
 
-        <img
-          src={require("../assets/google.png")}
-          alt="Logo Google"
-          className="parceiros-logo logo-google"
-        />
+          <img
+            src={require("../assets/google.png")}
+            alt="Logo Google"
+            className="parceiros-logo logo-google"
+          />
 
-        <img
-          src={require("../assets/optidata.png")}
-          alt="Logo Optidata"
-          className="parceiros-logo logo-optidata"
-        />
+          <img
+            src={require("../assets/optidata.png")}
+            alt="Logo Optidata"
+            className="parceiros-logo logo-optidata"
+          />
+        </div>
       </section>
     </>
   );
