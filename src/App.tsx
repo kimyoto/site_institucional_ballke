c628491cf98db7cdd95a7d3ff78337a0c25c1991
AdailtonZ <adailtonadriano3@gmail.com>
2025-12-02 16:22:32 -0300

    correção da importação do header em app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ConhecaPage from "./pages/ConhecaPage";
