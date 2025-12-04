520b6693b6abf3492cf8483d398b4deb5d79eec2
AdailtonZ <68255504+kimyoto@users.noreply.github.com>
2025-12-04 15:23:49 -0300

    feat: página 'conheça' atualizada pra consumir da API django

--- src/pages/ConhecaPage.tsx
+++ src/pages/ConhecaPage.tsx
@@ -1,47 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ConhecaPage.css";
 
+interface ConhecaHeroSection {
+  hero_bg_img: string | null;
+  hero_text_linha1: string | null;
+  hero_text_linha2: string | null;
+}
+
+interface ConhecaContentSection {
+  content_paragraph1: string | null;
+  content_paragraph2: string | null;
+  content_paragraph3: string | null;
+}
+
+interface ConhecaTimelineSection {
+  timeline_img: string | null;
+}
+
+interface ConhecaPageDate {
+  hero_section: ConhecaHeroSection;
+  content_section: ConhecaContentSection;
+  timeline_section: ConhecaTimelineSection;
+}
+
 function ConhecaPage() {
+  const [content, setContent] = useState<ConhecaPageDate | null>(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/conheca")
+      .then((response) => response.json())
+      .then((data: ConhecaPageDate) => setContent(data))
+      .catch((error) => {
+        console.error("Erro ao buscar dados da página Conheça:", error);
+        setContent({
+          hero_section: {
+            hero_bg_img: require("../assets/conhecatopo.png"),
+            hero_text_linha1: "Nossa trajetória",
+            hero_text_linha2: "até aqui.",
+          },
+          content_section: {
+            content_paragraph1: "A história do Grupo Ballke é uma jornada construída com dedicação, inovação e compromisso com a excelência. Desde o seu início, buscamos transformar a saúde e o bem-estar das pessoas, oferecendo soluções eficientes e de alta qualidade nos segmentos em que atuamos.",
+            content_paragraph2: "A cada novo desafio, reafirmamos nosso compromisso em entregar produtos e serviços que superem as expectativas de nossos clientes, sempre com foco no cuidado e na segurança. Ao longo de mais de 20 anos de atuação, o Grupo Ballke tem se destacado pela sua capacidade de adaptação e evolução, buscando constantemente aprimorar seus processos e garantir o melhor em cada entrega.",
+            content_paragraph3: "Agora, ampliamos nosso olhar para contribuir ativamente com a inovação em diferentes áreas do mercado. Nossa missão é apoiar empresas e profissionais na superação de desafios, promovendo soluções estratégicas, tecnológicas e sustentáveis que gerem impacto real — na saúde e além dela.",
+          },
+          timeline_section: {
+            timeline_img: require("../assets/lona.png"),
+          }
+        });
+      });
+  }, []);
+
+  if (!content) return <p>Carregando...</p>;
+  
+  const heroStyle = {
+    backgroundImage: `
+      linear-gradient(rgba(17, 17, 17, 0.5), rgba(24, 24, 24, 0.5)),
+      url(${content.hero_section.hero_bg_img})
+    `,
+  };
+
   return (
     <>
-      <section className="section-container conheca-hero-background">
+      <section className="section-container conheca-hero-background" style={heroStyle}>
         <h2 className="topo-text-1">
-          Nossa trajetória
+          {content.hero_section.hero_text_linha1}
           <br />
-          até aqui.
+          {content.hero_section.hero_text_linha2}
         </h2>
       </section>
 
       <section className="section-container conheca-main-background">
         <h2 className="conheca-text">
-          A história do Grupo Ballke é uma jornada construída com dedicação,
-          inovação e compromisso com a excelência. Desde o seu início, buscamos
-          transformar a saúde e o bem-estar das pessoas, oferecendo soluções
-          eficientes e de alta qualidade nos segmentos em que atuamos.
+          {content.content_section.content_paragraph1}
         </h2>
 
         <h2 className="conheca-text">
-          A cada novo desafio, reafirmamos nosso compromisso em entregar
-          produtos e serviços que superem as expectativas de nossos clientes,
-          sempre com foco no cuidado e na segurança. Ao longo de mais de 20 anos
-          de atuação, o Grupo Ballke tem se destacado pela sua capacidade de
-          adaptação e evolução, buscando constantemente aprimorar seus processos
-          e garantir o melhor em cada entrega.
+          {content.content_section.content_paragraph2}
         </h2>
 
         <h2 className="conheca-text">
-          Agora, ampliamos nosso olhar para contribuir ativamente com a inovação
-          em diferentes áreas do mercado. Nossa missão é apoiar empresas e
-          profissionais na superação de desafios, promovendo soluções
-          estratégicas, tecnológicas e sustentáveis que gerem impacto real — na
-          saúde e além dela.
+          {content.content_section.content_paragraph3}
         </h2>
       </section>
-              <img
-          src={require("../assets/lona.png")}
+      {content.timeline_section.timeline_img && (
+        <img
+          src={content.timeline_section.timeline_img}
           alt="Imagem Lona Conheça"
           className="conheca-lona-image"
-        />
+      />
+      )}
     </>
   );
 }
